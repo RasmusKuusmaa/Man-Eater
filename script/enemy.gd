54d75754bc04f705798f9c41fdcddba4db50dcb3
Rasmus Kuusmaa <77f315@gmail.com>
2026-05-01 22:01:30 +0300

    enemy chases player

---
+++ script/enemy.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var SPEED: float = 100
+var player: Node2D
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	
+func _physics_process(delta: float) -> void:
+	if player == null:
+		print("no palyer")
+		return
+		
+	var direction = (player.global_position - global_position).normalized()
+	velocity = direction * SPEED
+	move_and_slide()
