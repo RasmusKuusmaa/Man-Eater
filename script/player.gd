3c3693c6d30dc017c64d433837b95bf58a4bef5d
77f315 <77f315@gmail.com>
2026-05-03 18:53:17 +0300

    replace movement controls with custom ones

--- script/player.gd
+++ script/player.gd
@@ -7,14 +7,14 @@ var scale_cap:float = 20.0
 func _physics_process(delta: float) -> void:
 	var direction = Vector2.ZERO
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 		
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_down"):
 		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
 		
 	direction = direction.normalized()
